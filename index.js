49b217d3ce710378876026538be7f2864c5e76e3
Evan  Markert <evanmarkert@gmail.com>
2019-08-02 04:00:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return "This one is on me!"
-  } if (distance > 2000) {}
+  } if (distance > 2000) {
       return "I will gladly take your thirty bucks."
   }
 
