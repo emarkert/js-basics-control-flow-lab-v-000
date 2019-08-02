f8da9dcfc9f11d13eba6c255f88541f304be9fdc
Evan  Markert <evanmarkert@gmail.com>
2019-08-02 03:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+
+}
+
+fucntion ternaryCheckCity() {
+
+
+}
+
+function switchOnCharmFromTip() {
+
+
+}
